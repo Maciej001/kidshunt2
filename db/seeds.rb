62f3ad76b4c9a4896ec2d8f26bfcdd977716fce3
Maciej Nowakowski <m.nowakowskipl@gmail.com>
2014-11-09 20:18:45 +0000

    css added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-Post.create(title: "Code", tagline: "Learn to code to make history", url: "http://code.org")
-Post.create(title: "NASA for Kids", tagline: "Discover the universe", url: "http://science.nasa.gov")
-Post.create(title: "Useless WEB", tagline: "Dive into alternative useless WEB", url: "http://theuselessweb.com")
+Post.create(title: "Code", tagline: "Learn to code to make history", url: "http://code.org", upvotes: 7)
+Post.create(title: "NASA for Kids", tagline: "Discover the universe", url: "http://science.nasa.gov", upvotes: 51)
+Post.create(title: "Useless WEB", tagline: "Dive into alternative useless WEB", url: "http://theuselessweb.com", upvotes: 14)
